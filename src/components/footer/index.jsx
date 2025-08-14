ce33ac581d059ee8bda4e0e536a35f28edbae068
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2025-08-14 11:59:38 +0200

    Project initialization

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.container}>
+            <h1>
+                My footer
+            </h1>
+        </footer>
+    );
+}
