d02c2e81a88cd4ea17fdd6e10ff0f8a3d18fd80d
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2025-08-14 17:18:40 +0200

    OMG I'm so happy, it works

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import NavSidebar from "./components/nav-sidebar";
 import {useState} from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import sidebarStyles from "./components/nav-sidebar/nav-sidebar.module.css"
 
 function App() {
     const [sidebarToggle, setSidebarToggle] = useState(false);
