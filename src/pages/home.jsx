4ae69e9c24ecb117081083a73b7269c9c46e1401
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2025-08-15 11:05:50 +0200

    Home page connected

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import ContactsMaps from "../components/home/contact-maps";
+import HeroSection from "../components/home/hero-section";
 
 export default function Home() {
 
     return (
         <main>
+            <HeroSection/>
             <ContactsMaps />
         </main>
     );
