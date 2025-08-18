9814d95b391fbf76f00821df6e76ecc735c3c4e0
Maksym Furmanov <maksym.furmanov@gmail.com>
2025-08-18 14:52:33 +0200

    Background video changed format

--- src/components/home/hero-section/index.jsx
+++ src/components/home/hero-section/index.jsx
@@ -1,25 +1,15 @@
 import styles from "./hero-section.module.css";
-import {useEffect, useRef} from "react";
 
 export default function HeroSection() {
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.setAttribute("playsinline", "");
-        }
-    }, []);
-
     return (
         <section className={styles.container}>
-            <video ref={videoRef}
-                autoPlay
+            <video autoPlay
                    muted
                    loop
                    playsInline={true}
                    className={styles.bgVideo}
             >
-                <source src="/videos/bg-kosice-1080.mp4"
+                <source src="/videos/bg-kosice-1080.av1.mp4"
                         type="video/mp4"
                 />
             </video>
