9872ec168b9c1ab3a7f2b1672c3d44f713a198d9
Maksym Furmanov <maksym.furmanov@gmail.com>
2025-08-22 09:02:46 +0200

    Hero section and header styles fixed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,7 @@ import navLinks from "../../lib/nav-links";
 import {useNavigate} from "react-router-dom";
 import {RiMenu2Line} from "react-icons/ri";
 import {IoMdClose} from "react-icons/io";
-import { ReactComponent as Logo } from '../../assets/logo-white.svg';
+import {ReactComponent as Logo} from '../../assets/logo-white.svg';
 import {useTranslation} from "react-i18next";
 
 export default function Header({sidebarToggle, setSidebarToggle}) {
@@ -34,30 +34,32 @@ export default function Header({sidebarToggle, setSidebarToggle}) {
 
     return (
         <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
-            <div className={styles.logo}>
-                <Logo/>
-            </div>
-            <nav className={styles.links}>
-                {navLinks.map((link, index) => {
-                    return (
-                        <p key={index}
-                           onClick={() =>
-                               navigate(link.url)
-                           }>
-                            {t(link.label)}
-                        </p>
-                    )
-                })}
-            </nav>
-            <div className={styles.burgerIconContainer}>
-                {sidebarToggle
-                    ? <IoMdClose onClick={() => {
-                        closeSidebar()
-                    }}/>
-                    : <RiMenu2Line onClick={() => {
-                        openSidebar()
-                    }}/>
-                }
+            <div className={styles.wrapper}>
+                <div className={styles.logo}>
+                    <Logo/>
+                </div>
+                <nav className={styles.links}>
+                    {navLinks.map((link, index) => {
+                        return (
+                            <p key={index}
+                               onClick={() =>
+                                   navigate(link.url)
+                               }>
+                                {t(link.label)}
+                            </p>
+                        )
+                    })}
+                </nav>
+                <div className={styles.burgerIconContainer}>
+                    {sidebarToggle
+                        ? <IoMdClose onClick={() => {
+                            closeSidebar()
+                        }}/>
+                        : <RiMenu2Line onClick={() => {
+                            openSidebar()
+                        }}/>
+                    }
+                </div>
             </div>
         </header>
     );
