e87b7852689c0f4109acc16bd04386c93bf9df4a
Maksym Furmanov <furmanov.maksym@gmail.com>
2025-12-02 12:34:00 +0100

    Website owners rights added

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
 
                 {/* копірайт */}
                 <div className={styles.copyRow}>
-                    <span>Text © {year} Košice</span>
+                    <span> © {year} Magistrát mesta Košice. Všetky práva vyhradené</span>
                 </div>
 
             </div>
