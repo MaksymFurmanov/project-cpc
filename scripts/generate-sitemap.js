061126ec91e9b5defbd40be3df1e215ac8eb10ea
Maksym Furmanov <furmanov.maksym@gmail.com>
2025-12-09 10:15:12 +0100

    Sitemap generation logic added

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,21 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+
+async function generateSitemap() {
+    const sitemap = new SitemapStream({ hostname: "https://kosiceforyou.sk/" });
+
+    const pages = [
+        '/',
+    ];
+
+    pages.forEach((route) => {
+        sitemap.write({ url: route, changefreq: 'monthly' });
+    });
+
+    sitemap.end();
+
+    const data = await streamToPromise(sitemap);
+    createWriteStream('./public/sitemap.xml').write(data.toString());
+}
+
+generateSitemap();
