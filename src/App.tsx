190abe787f306e876e16d6dd7eb8db5b58b484e5
maksym <furmanov.maksym@gmail.com>
2026-03-12 08:41:45 +0100

    Vercel speed insides

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
         <>
             <RouterProvider router={router} />
             <Analytics/>
+            <SpeedInsights/>
         </>
     );
 }
