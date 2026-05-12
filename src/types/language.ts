487b0730df9be55b7a0ba5621411d39f5baccd67
maksym <furmanov.maksym@gmail.com>
2026-05-12 10:16:14 +0200

    Type language has been simplified

--- src/types/language.ts
+++ src/types/language.ts
@@ -1,10 +1,7 @@
-import {languages} from "../utils";
-
 export type LanguageOption = {
     label: string,
     flagImg: string,
     systemName: string
 }
 
-type LanguagesObject = (typeof languages)[number];
-export type Language = LanguagesObject["systemName"];
+export type Language = "sk" | "en" | "uk";
